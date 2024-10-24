a6012e527ce04ad409f274c56e92bbc439b4d98f
carlos larreal <carloslarrealmorales@gmail.com>
2024-10-23 23:06:35 -0300

    React Router I

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './componentes/Navbar';
-//import RegisterPage from './componentes/RegisterPage';
-//import LoginPage from './componentes/login';
-//import Home from './componentes/Home';
 import Footer from './componentes/Footer';
-import Pizza from './componentes/Pizza';
-//import Cart from "./componentes/cart";
+import Home from './pages/Home';
+import Register from './pages/RegisterPage';
+import Login from './pages/Login';
+import Cart from './pages/cart';
+import Pizza from './pages/Pizza';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
+
+
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Navbar />
-      {/*<Home /> */}
-      {/* <RegisterPage/> */}
-      {/*<LoginPage /> */}
-      {/*<Cart />*/}
-      <Pizza/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/pizza/p001" element={<Pizza />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const total = 25000;
@@ -6,41 +7,29 @@ const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#191919' }}>
-      <a className="navbar-brand text-white m-2 text-decoration-none" href="#">🍕 Pizzería Mamma Mia!</a>
+      <Link className="navbar-brand text-white m-2 text-decoration-none" to="/">🍕 Pizzería Mamma Mia!</Link>
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <button className="btn btn-outline-secondary m-2 btn-sm">🍕 Home</button>
+            <Link className="btn btn-outline-secondary m-2 btn-sm" to="/">🍕 Home</Link>
           </li>
-          {token ? (
-            <>
-              <li className="nav-item">
-                <button className="btn btn-outline-secondary m-2 btn-sm">🔓 Profile</button>
-              </li>
-              <li className="nav-item">
-                <button className="btn btn-outline-secondary m-2 btn-sm">🔒 Logout</button>
-              </li>
-            </>
-          ) : (
-            <>
-              <li className="nav-item">
-                <button className="btn btn-outline-secondary m-2 btn-sm">🔐 Login</button>
-              </li>
-              <li className="nav-item">
-                <button className="btn btn-outline-secondary m-2 btn-sm">🔐 Register</button>
-              </li>
-            </>
-          )}
-         
+          <li className="nav-item">
+            <Link className="btn btn-outline-secondary m-2 btn-sm" to="/login">🔐 Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="btn btn-outline-secondary m-2 btn-sm" to="/register">🔐 Register</Link>
+          </li>
         </ul>
       </div>
-      <div class="d-flex justify-content-end">
-            <li className="nav-item">
-            <button className="btn btn-outline-info m-2 btn-sm">🛒 Total: ${total.toLocaleString()}</button>
-            </li>
+      <div className="d-flex justify-content-end">
+        <li className="nav-item">
+          <Link className="btn btn-outline-info m-2 btn-sm" to="/cart">🛒 Total: ${total.toLocaleString()}</Link>
+        </li>
       </div>
     </nav>
   );
 };
 
 export default Navbar;
+
+
